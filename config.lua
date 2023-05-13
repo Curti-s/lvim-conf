f2011095486f7f3dcde114bb2abe72f443f95822
curti-s <matthewscurtis.dev81@gmail.com>
2023-05-13 14:11:27 +0300

    after install / manual lunar vim update comment out dap-vs-code plugin track lazy-lock

--- config.lua
+++ config.lua
@@ -1,3 +1,7 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
 --[[
 lvim is the global options object
 
@@ -214,16 +218,16 @@ dap.adapters.node = {
   command = 'node'
 }
 
-require("dap-vscode-js").setup({
-  -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-  debugger_path = "~/.local/share/lunarvim/site/pack/packer/opt/vscode-js-debug",
-  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
-  -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-  -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-  log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-})
+-- require("dap-vscode-js").setup({
+--   -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+--   -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+--   debugger_path = "~/.local/share/lunarvim/site/pack/packer/opt/vscode-js-debug",
+--   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+--   adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
+--   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+--   -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+--   log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+-- })
 
 for _, language in ipairs({ "typescript", "javascript" }) do
   require("dap").configurations[language] = {
