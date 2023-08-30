0d0457b3d1f1630adea86fcbc1a21f17dae68f74
curti-s <matthewscurtis.dev81@gmail.com>
2023-08-30 21:18:56 +0300

    set colorscheme to lunar

--- config.lua
+++ config.lua
@@ -21,7 +21,7 @@ vim.opt.colorcolumn = '100'
 
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = false
-lvim.colorscheme = "gruvbox"
+lvim.colorscheme = "lunar"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
